b35e0d1a2fa2d674dc191eb9c61cda2772443a5c
Eugene Kalinin <kalinin.eugene@gmail.com>
2014-11-26 10:30:28 +0300

    Added PostgreSQL JDBC driver

--- JDBC.SOURCE.R
+++ JDBC.SOURCE.R
@@ -1,8 +1,13 @@
-
-Sys.setenv(JAVA_HOME='/usr/java/jdk1.7.0_67')
+# JAVA_HOME
+Sys.setenv(JAVA_HOME='/usr/java/default')
 options(java.parameters="-Xmx2g")
 library(rJava)
+
 
 # Output Java version
 .jinit()
 print(.jcall("java/lang/System", "S", "getProperty", "java.version"))
+
+
+# Load RJDBC library
+library(RJDBC)
