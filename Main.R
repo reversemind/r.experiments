27a227c437135ed3620b4bf0d62e41f0315285da
Eugene Kalinin <kalinin.eugene@gmail.com>
2014-12-12 20:28:02 +0300

    readed all data into RAM

--- Main.R
+++ Main.R
@@ -9,80 +9,59 @@ fileList <-list.files(DIRECTORY)
 print("Files:")
 length(fileList)
 
+
+N <- length(fileList)
+
 a<-list()
-for(i in 1:length(fileList)){
+for(i in 1:N){
   print(paste("",i,"=",fileList[i], sep = ""))
+  
   fileName <- paste(DIRECTORY,SEPARATOR,fileList[i], sep = "")
+  
   aTmp<-read.csv(fileName)
+  
   a[[i]]<-aTmp
   names(a[[i]])
 }
+
+
+index<-1
+value<-length(a[[index]]$Date)
+
+for(i in 1:N){
+  if(length(a[[i]]$Date)<value){
+    value<-length(a[[i]]$Date)
+    index<-i
+  }
+}
+
+
+shortestIndex<-index
+print(paste("Min Length in:",shortestIndex,sep = ""))
+
+
+class(length(a[[shortestIndex]]))
 
 
 
 length(a)
 names(a[[1]])
 
-
 length(a[[1]]$Date)
-length(a[[2]]$Date)
+class(a[[2]]$Date[1])
+
+a[[1]]$Date[2]
+
+# All values
+a[[shortestIndex]]$sulfate
 
 
 
 
-class(a[[2]])
-
-names(a[1])
-
-
-length(a[1])
-length(a[[1]])
-
-
-class(a[[1]])
-
-a<-list()
-a1 = read.csv(paste(DIRECTORY,SEPARATOR,"331.csv", sep = ""))
-a2 = read.csv(paste(DIRECTORY,SEPARATOR,"332.csv", sep = ""))
-
-a<-c(a,"1" = a1)
-a<-c(a,"2" = a2)
-
-names(a)
-
-
-
-a<-c(a,a1, "a2" = a2)
-class("a2")
-
-a<-data.frame()
-a<-data.frame(a1,a2)
-
-
-length(a)
-
-names(a)
-a[[2]]
-
-m<-matrix(nrow=4,)
 
 
 
 
-names(a[9])
-
-names(a[8])
-
-#----------------
-for(i in 1:length(fileList)){
-  print(paste("",i,"=",fileList[i], sep = ""))
-  _a<-read.csv(paste(DIRECTORY,SEPARATOR,fileList[i], sep = ""))
-  if(length(_a) >0){
-    a<-c(a,paste("\"",i,"\"",sep = "")=_a)  
-  }
-}
-
-air = read.csv(paste(DIRECTORY,SEPARATOR,"332.csv", sep = ""))
 
 
 # Dates - experiments
